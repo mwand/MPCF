d3809b3cc727742a8fc9db7481fc9d33172a6e8e
Mitchell Wand <wand@ccs.neu.edu>
2016-05-04 15:42:43 -0400

    Correct version of Adequacy.v installed

--- icfp-2016/Adequacy.v
+++ icfp-2016/Adequacy.v
@@ -24,18 +24,31 @@ Definition strict  {X: Type} {Y : Type} (v :lifted X)  (f : X -> (lifted Y)) : (
     | lift x => (f x)
   end.
 
+Inductive Unit : Type :=
+| unitval : Unit.
+
+(* Fake functions *)
+Definition left_half (u : Unit) := u.
+Definition right_half (u : Unit) := u.
+Definition uniform_dist (u : Unit) := u.
+
+Inductive real : Type :=
+| unit_real : Unit -> real.
+
 (* Object Types *)
 
 Inductive Otype : Type :=
-| Inttype : Otype              
-| Arrtype : Otype -> Otype -> Otype.
+| Funtype : Otype -> Otype -> Otype
+| Realtype : Otype
+| Meastype : Otype -> Otype.
 
 (* Denotations of object types *)
 
 Function den_type (t : Otype) : Type :=
   match t with
-    | Inttype => nat
-    | Arrtype t u => (den_type t) -> (lifted (den_type u))
+    | Funtype t u => (den_type t) -> (lifted (den_type u))
+    | Realtype => real
+    | Meastype t => Unit -> lifted (den_type t)
   end.
 
 (* **************** Environments **************** *)
@@ -96,13 +109,22 @@ Definition apply_env
 Inductive Exp G : Otype -> Type :=
 | valexp : forall o, Val G o -> Exp G o
 | appexp : forall (a b : Otype),
-             (Exp G (Arrtype a b)) -> (Exp G a) -> (Exp G b) 
+             (Exp G (Funtype a b)) -> (Exp G a) -> (Exp G b)
+| bindexp : forall o o', Exp G (Meastype o) ->
+                        Exp G (Funtype o (Meastype o')) ->
+                        Exp G (Meastype o')
+| returnexp : forall o, Exp G o -> Exp G (Meastype o)
 with
 Val G : Otype -> Type :=
-| constexp : nat -> Val G Inttype
+| constexp : real -> Val G Realtype
 | varexp : forall o, Var G o -> Val G o 
 | absexp : forall (o o': Otype),
-             (Exp (cons o G) o') -> (Val G (Arrtype o o')).
+             (Exp (cons o G) o') -> (Val G (Funtype o o'))
+| uniformval : Val G (Meastype Realtype)
+| bindval : forall o o', Val G (Meastype o) ->
+                        Val G (Funtype o (Meastype o')) ->
+                        Val G (Meastype o')
+| returnval : forall a, Val G a -> Val G (Meastype a).
 
 (* Coq doesn't generate the correct mutual induction hypotheses
    for these, so we have to help it along. *)
@@ -118,18 +140,39 @@ Function cbv  {A : Type}{B : Type}
                  (d': lifted A) : lifted B :=
   strict d (fun f => strict d' (fun a => (f a))).
 
+Function bindop {a b}
+         (s : den_type (Meastype a))
+         (f : den_type (Funtype a (Meastype b)))
+         : (den_type (Meastype b)) :=
+  (fun (u:Unit) =>
+     (strict
+        (s (left_half u))
+        (fun a =>
+           (strict
+              (f a)
+              (fun s2 => (s2 (right_half u))))))).
+
+Function returnop {a} (v : den_type a) : (den_type (Meastype a))
+  := fun u => (lift v).
+
+Definition uniformop : (den_type (Meastype Realtype))
+  := fun u => (lift (unit_real u)).
+
+
 (* Coercion, needed to make den_val typecheck *)
-Function coerce_int (n : nat) : (den_type Inttype) := n.
+Function coerce_real (w : real) : (den_type Realtype) := w.
 
 (* Denotations of Values and Expressions *)
 
 Function den_val {G o} (val : Val G o) r : (den_type o)
   :=
     match val with
-      | constexp n => (coerce_int n)
+      | constexp w => (coerce_real w)
       | varexp  _ var => (apply_env r var)
-   (* | absexp o' o e => (fun val => den_exp e (val, r)) *)
-      | absexp o' o e => (fun val => den_exp e (cons_env val r))                    
+      | absexp o' o e => (fun val => den_exp e (cons_env val r))
+      | uniformval => uniformop
+      | bindval _ _ s f => bindop (den_val s r) (den_val f r)
+      | returnval _ v => returnop (den_val v r)
     end
 with
 den_exp {G o} (exp : (Exp  G o)) (r : (den_env G)) : (lifted (den_type o))
@@ -137,6 +180,12 @@ den_exp {G o} (exp : (Exp  G o)) (r : (den_env G)) : (lifted (den_type o))
   match exp with
     | valexp _ val => lift (den_val val r)
     | appexp _ _ e1 e2 => cbv (den_exp e1 r) (den_exp e2 r)
+    | returnexp _ e1 => strict (den_exp e1 r)
+                               (fun a => (lift (returnop a)))
+    | bindexp _ _ e1 e2
+      => strict (den_exp e1 r)
+                (fun s => strict (den_exp e2 r)
+                                 (fun f => (lift (bindop s f))))
   end.
 
 (* **************** Substitutions **************** *)
@@ -242,6 +291,9 @@ Fixpoint ap_Rv {G G' o} (ren : Renaming G G')
     | varexp _ var => varexp (ren _ var)
     | absexp _ _ e => absexp (ap_Re (pad_renaming ren) e)
     | constexp n => @constexp _ n 
+    | uniformval => @uniformval _
+    | bindval _ _ e1 e2 => bindval (ap_Rv ren e1) (ap_Rv ren e2)
+    | returnval _ v => returnval (ap_Rv ren v)
   end
 with
 ap_Re {G G' o} (ren : Renaming G G') (exp : Exp G o)
@@ -249,7 +301,11 @@ ap_Re {G G' o} (ren : Renaming G G') (exp : Exp G o)
   match exp with
     | valexp _ val => valexp (ap_Rv ren val)
     | appexp _ _ e1 e2 => appexp (ap_Re ren e1) (ap_Re ren e2)
+    | bindexp _ _ e1 e2 => bindexp (ap_Re ren e1) (ap_Re ren e2)
+    | returnexp _ e1 => returnexp (ap_Re ren e1)
   end.
+
+Hint Resolve ap_Rv ap_Re.
 
 (* GIVEN: subst : Subst G G',  *)
 (* RETURNS: a new substitution, with var0 bound to var0:o, and 
@@ -274,6 +330,9 @@ Fixpoint ap_Sv G G' o (s : Subst G G') (val : Val G o) : (Val G' o)
       | constexp n => constexp G' n
       | varexp _ var => subst_lookup var s
       | absexp o o' e => absexp (ap_Se (shift_subst s) e)
+      | uniformval => @uniformval _
+      | bindval _ _ e1 e2 => bindval (ap_Sv s e1) (ap_Sv s e2)
+      | returnval _ v => returnval (ap_Sv s v)
     end
 with
 ap_Se G G' o (s : Subst G G') (e : Exp G o) : (Exp G' o)
@@ -281,9 +340,13 @@ ap_Se G G' o (s : Subst G G') (e : Exp G o) : (Exp G' o)
   match e with
     | valexp _ val => valexp (ap_Sv s val)
     | appexp _ _ e1 e2 => appexp (ap_Se s e1) (ap_Se s e2)
+    | bindexp _ _ e1 e2 => bindexp (ap_Se s e1) (ap_Se s e2)
+    | returnexp _ e1 => returnexp (ap_Se s e1)
   end.
 
-(* compose renamings and compositions.  All are right-to-left *)
+Hint Resolve ap_Sv ap_Se.
+
+(* compositions of renamings and substitutions.  All are right-to-left *)
 
 Definition c_RR {E E' E''} (r : Renaming E' E'') (r' : Renaming E E') := 
   (fun t v => r t (r' t v)) : Renaming E E''.
@@ -334,28 +397,16 @@ Lemma ap_Sv_id :
 Proof.
   apply (val_exp_rec
            (fun E t (e : Exp E t) => ap_Se id_subst e = e)
-           (fun E t (v : Val E t) => ap_Sv id_subst v = v)).
-  intros.
-  simpl.
-  rewrite H.
-  reflexivity.
+           (fun E t (v : Val E t) => ap_Sv id_subst v = v));
+  intros; auto.
 
-  intros. simpl.
-  rewrite H.
-  rewrite H0.
-  reflexivity.
-
-  intros.
-  auto.                         (* does simpl; reflexivity *)
-
-  intros.
-  auto.  (* does simpl; unfold*; reflexivity *)
-
-  intros.
-  simpl.
-  rewrite shift_id_subst.
-  rewrite H.
-  auto.
+  simpl. rewrite H.  auto.
+  simpl. rewrite H. rewrite H0.  auto.
+  simpl. rewrite H. rewrite H0. auto.
+  simpl. rewrite H. auto.
+  simpl. rewrite shift_id_subst. rewrite H. auto.
+  simpl. rewrite H. rewrite H0. auto.
+  simpl. rewrite H. auto.
 Qed.
 
 (* pad distributes over composition of renamings *)
@@ -511,8 +562,6 @@ Proof.
   reflexivity.
 Qed.
 
-
-
 (* **************************************************************** *)
 
 (* And, at last, the operational semantics!!!! *)
@@ -520,23 +569,45 @@ Qed.
 
 Inductive ev : forall (o: Otype), Exp nil o -> Val nil o -> Prop :=
   | ev_val : forall o (val: Val nil o), ev (valexp val) val
-  | ev_app : forall o o' (e1 : Exp nil (Arrtype o o'))
+  | ev_app : forall o o' (e1 : Exp nil (Funtype o o'))
                     (e2 : Exp nil o)
                     e' v w,
                ev e1 (absexp e')
                -> ev e2 v
                -> ev (ap_Se (subst1 v) e') w
-               -> ev (appexp e1 e2) w.
+               -> ev (appexp e1 e2) w
+  | ev_bind : forall a b (e1 : Exp nil (Meastype a))
+                     (e2 : Exp nil (Funtype a (Meastype b)))
+                     (c1 : Val nil (Meastype a))
+                     (c2 : Val nil (Funtype a (Meastype b))),
+                ev e1 c1 ->
+                ev e2 c2 ->
+                ev (bindexp e1 e2) (bindval c1 c2)
+  | ev_return : forall a (e : Exp nil a) (c : Val nil a),
+                  ev e c ->
+                  ev (returnexp e) (returnval c).
 
-
-
+Inductive evs : forall (o: Otype),
+                  Unit -> Val nil (Meastype o) -> Val nil o -> Prop :=
+| evs_unif : forall u,
+               evs u (uniformval nil) (constexp _ (unit_real u))
+| evs_return : forall o u (c: Val nil o),
+                 evs u (returnval c) c
+| evs_bind : forall u a b (c1 : Val nil (Meastype a))
+                    (c2 : Val nil (Funtype a (Meastype b)))
+                    c3 c4 c5,
+               evs (left_half u) c1 c3 ->
+               ev (appexp (valexp c2) (valexp c3)) c4 ->
+               evs (right_half u) c4 c5 ->
+               evs u (bindval c1 c2) c5.
+               
 (* **************************************************************** *)
 
 (* The Logical Relation *)
 
 (* Defined as a fixed point over Otypes and lifted Otypes *)
 
-Definition EqInt : (den_type Inttype) -> nat -> Prop :=
+Definition EqReal : (den_type Realtype) -> real -> Prop :=
   fun n1 n2 => (n1 = n2).
 
 (* General recipe for lifting a relation *)
@@ -555,24 +626,34 @@ Definition  FunRel {o1 o2}
             (Rel1 : (den_type o1) -> (Val [] o1) -> Prop)
             (Rel2 : (lifted (den_type o2)) -> (Exp [] o2) -> Prop)
             (f1 : (den_type o1) -> (lifted (den_type o2)))
-            (val1 : Val nil (Arrtype o1 o2))
+            (val1 : Val nil (Funtype o1 o2))
 : Prop :=
   exists e, val1 = absexp e /\
   forall (a1 : den_type o1) (val2: Val [] o1),
     (Rel1 a1 val2) ->
     (Rel2 (f1 a1) (ap_Se (subst1 val2) e)).
 
-(* for some reason, Coq thinks this makes the first argument to Rel implicit  *)
+Definition MeasRel {o}
+           (Rel : (den_type o) -> (Val [] o) -> Prop)
+           (s : den_type (Meastype o))
+           (c : Val nil (Meastype o)) : Prop :=
+  forall (u:Unit),
+    match (s u) with
+      | bottom => True
+      | lift a => exists c', evs u c c' /\ Rel a c'
+    end.
+
 Fixpoint Rel (o : Otype) : (den_type o) -> (Val nil o) -> Prop :=
  match o with
-    | Inttype => fun d val => match val with
-                       | constexp n => EqInt d n
-                       | _ => False
-                     end
-    | Arrtype o1 o2 =>
-      fun (f1 : den_type (Arrtype o1 o2))
-           (val1 : Val nil (Arrtype o1 o2)) =>
+     | Realtype => fun d val => match val with
+                                  | constexp n => EqReal d n
+                                  | _ => False
+                                end
+     | Funtype o1 o2 =>
+       fun (f1 : den_type (Funtype o1 o2))
+           (val1 : Val nil (Funtype o1 o2)) =>
          FunRel (@Rel o1)  (lift_Rel (@Rel o2)) f1 val1
+    | Meastype o => MeasRel (@Rel o)
  end.
 
 
@@ -621,6 +702,125 @@ Proof.
   apply H.
 Qed.
 
+Lemma bind_preserves_Rel :
+  forall o o' (s1 : Unit -> lifted (den_type o))
+         (f : den_type o -> lifted (Unit -> lifted (den_type o')))
+         c1 c2,
+    MeasRel (Rel (o:=o)) s1 c1 ->
+    FunRel (Rel (o:=o)) (lift_Rel (MeasRel (Rel (o:=o')))) f c2 ->
+    MeasRel (Rel (o:=o')) (bindop s1 f) (bindval c1 c2).
+Proof.    
+  intros.
+  unfold MeasRel.
+  intros u.
+  destruct (bindop s1 f u) as [| b] eqn: f_u; auto.
+  unfold bindop in f_u.
+  destruct (s1 (left_half u)) as [| a] eqn: s1_uL; auto.
+
+  (* (s1 u_L) = bottom *)
+  simpl in f_u; inversion f_u.
+
+  (* (s1 u_L) = lift a *)
+  destruct (f a) as [| s'] eqn: f_a; auto.
+  simpl in f_u.
+  rewrite f_a in f_u.
+  simpl in f_u.
+  inversion f_u.
+
+  (* (f a) = lift s' *)
+  destruct (s' (right_half u)) as [| b'] eqn: s'_uR; auto.
+
+  (* (s' u_R) = bottom *)
+  simpl in f_u.
+  rewrite f_a in f_u.
+  simpl in f_u.
+  rewrite s'_uR in f_u.
+  inversion f_u.
+
+  (* (s' u_R) = lift b *)
+  simpl in f_u.
+  rewrite f_a in *.
+  simpl in f_u.
+
+  assert (Hc3: exists c3, (evs (left_half u) c1 c3) /\ Rel a c3).
+  { rewrite s'_uR in *.
+    inversion f_u.
+    rewrite H2 in *.
+    clear f_u.
+    unfold FunRel in H0.
+    inversion H0 as [e2]. clear H0. inversion H1. clear H1.
+    unfold MeasRel in H.
+    specialize (H (left_half u)).
+    rewrite s1_uL in H.
+    inversion H as [c3]; clear H.
+    exists c3.
+    apply H1. }
+    
+  inversion Hc3 as [c3]; clear Hc3.
+  inversion H1; clear H1.
+  unfold FunRel in H0.
+  inversion H0 as [e2']; simpl; clear H0.
+  inversion H1; clear H1.
+  specialize (H4 a c3 H3).
+  unfold lift_Rel in H4.
+  rewrite f_a in H4.
+  inversion H4 as [c4]; clear H4.
+  inversion H1; clear H1.
+  unfold MeasRel in H5.
+  specialize (H5 (right_half u)).
+  rewrite s'_uR in *.
+  inversion H5 as [c5]; clear H5.
+  exists c5.
+  inversion H1; clear H1.
+  inversion f_u as [eq_b] ; clear f_u.
+  rewrite eq_b in *.
+  split.
+  assert (H10: ev (appexp (valexp c2) (valexp c3)) c4).
+  { rewrite H0 in *.
+    assert (H12: ev (valexp (absexp e2')) (absexp e2')) by apply ev_val.
+    assert (H13: ev (valexp c3) c3) by apply ev_val.
+    apply (ev_app H12 H13 H4).
+  }
+  apply (evs_bind H2 H10 H5).
+  (* woo-hoo! *)
+  apply H6.
+Qed.
+
+Hint Resolve bind_preserves_Rel.
+
+Lemma return_preserves_Rel :
+  forall o (v : den_type o) (c : Val nil o),
+         Rel v c ->
+         MeasRel (Rel (o:=o)) (returnop v) (returnval c).
+Proof.
+  intros.
+  unfold returnop.
+  unfold MeasRel.
+  intros.
+  exists c.
+  split.
+  apply evs_return.
+  apply H.
+Qed.
+
+Hint Resolve return_preserves_Rel.
+
+Lemma unif_preserves_Rel :
+  MeasRel (Rel (o:=Realtype)) uniformop (uniformval nil).
+Proof.
+  unfold MeasRel.
+  intros.
+  unfold uniformop.
+  exists (constexp _ (unit_real u)).
+  split.
+  apply evs_unif.
+  unfold Rel.
+  unfold EqReal.
+  auto.
+Qed.
+
+Hint Resolve unif_preserves_Rel.
+
 (* **************************************************************** *)
 
 (* The Fundamental Theorem *)
@@ -643,56 +843,27 @@ Definition Fun_Thm_Val G o   (val: Val G o):=
 
 Theorem Fund_Thm: forall G o exp, @Fun_Thm_Exp G o exp.
 Proof.
-  apply (exp_val_rec Fun_Thm_Exp Fun_Thm_Val).
-
-  (*valexp*)
-  intros.
-  unfold Fun_Thm_Exp.
-  intros.
+  apply (exp_val_rec Fun_Thm_Exp Fun_Thm_Val);
+  unfold Fun_Thm_Exp;
+  unfold Fun_Thm_Val;
+  intros;
   simpl.
+
+  (* valexp *)
+  specialize (H _ _ H0).
   exists (ap_Sv s v).
   split.
   apply ev_val.
-  { destruct v.
-    (* constexp *)
-    simpl.
-    unfold EqInt.
-    reflexivity.
-    (* varexp *)
-    simpl.
-    apply lookup_preserves_Rel.
-    apply H0.
-    (* absexp *)
-    apply H.
-    apply H0.
-    }
+  apply H.
+
   { (* appexp e1 e2 *)
     intros.
-    unfold Fun_Thm_Exp.
-    intros.
-    unfold Fun_Thm_Exp in H.
-    unfold Fun_Thm_Exp in H0.
     specialize (H _ _ H1).
     specialize (H0 _ _ H1).
-    simpl.
-    destruct (den_exp e r) eqn: eqe.
-    simpl in H.
-    destruct (den_exp e0 r) eqn: eqe0.
-    simpl.
-    apply I.
-    simpl.
-    apply I.
-    simpl.
-    destruct (den_exp e0 r) eqn: eqe0.
-    simpl.
-    apply I.
-    simpl.
-    destruct (d d0) eqn: eqb.
-    simpl.
-    apply I.
-    simpl.
-    simpl in H0.
-    simpl in H.
+    destruct (den_exp e r) eqn: eqe; simpl; auto.
+    destruct (den_exp e0 r) eqn: eqe0; simpl; auto.
+    destruct (d d0) eqn: eqb; simpl; auto.
+    simpl in *.
     inversion H. clear H.
     inversion H2. clear H2.
     inversion H3. clear H3.
@@ -702,61 +873,117 @@ Proof.
     specialize (H4 d0 x1 H5).
     rewrite eqb in *.
     simpl in H4.
-    inversion H4.
-    exists x2.
+    inversion H4 as [v].
+    exists v.
     rewrite H3 in *.
     split.
     apply (ev_app H H0).
     apply H6.
     apply H6.
   }
-  (* constexp *)
-  unfold Fun_Thm_Val.
-  intros.
+
+  (* bindexp e e0 *)
+  { specialize (H _ _ H1).
+    specialize (H0 _ _ H1).
+    destruct (den_exp e r) as [| s1] eqn: eq_e; auto.
+    destruct (den_exp e0 r) as [| f] eqn: eq_e0; auto.
+
+    (* e, e0 non-bottom *)
+    simpl.
+    simpl in *.  
+    inversion H as [c1]; clear H; inversion H2; clear H2.
+    inversion H0 as [c2]; clear H0; inversion H2; clear H2.
+    exists (bindval c1 c2). 
+    split.
+    apply ev_bind.
+    apply H.
+    apply H0.
+    apply bind_preserves_Rel.
+    apply H3.
+    apply H4.
+  }
+
+  (* returnexp e *)
+  specialize (H _ _ H0).
+  unfold lift_Rel.
+  destruct (den_exp e r) as [|v1] eqn: eq_e; auto.
   simpl.
-  unfold EqInt.
-  unfold coerce_int.
-  reflexivity.
+  unfold lift_Rel in H.
+  inversion H as [v]; clear H.
+  inversion H1; clear H1.
+  exists (returnval v).
+  split.
+  apply ev_return.
+  apply H.
+  apply return_preserves_Rel.
+  apply H2.
+
+  (* constexp *)
+  unfold coerce_real.
+  unfold EqReal.
+  auto.
 
   (* varexp *)
-  unfold Fun_Thm_Val.
-  intros.
-  simpl.
-  apply lookup_preserves_Rel.
-  apply H.
+  apply lookup_preserves_Rel. apply H.
 
-  (* absexp *)
+  { (* absexp *)
+  (* copied straight from adequacy-cbv.v! *)
+    intros.
+    unfold Fun_Thm_Val.
+    intros.
+    simpl.
+    unfold FunRel.
+    exists (ap_Se (shift_subst s) e).
+    split.
+    reflexivity.
+
+    intros.
+
+    assert (Rel_Env (cons_env a1 r) (cons_subst val2 s)).
+    apply extend_env_preserves_Rel.
+    apply H0.
+    apply H1.
+    unfold lift_Rel.
+    destruct (den_exp e (cons_env a1 r)) eqn: eq_e.
+    apply I.
+
+
+    specialize (H _ _ H2).
+    rewrite eq_e in *.
+    simpl in H.
+    inversion H as [v]; clear H.
+    exists v.
+    inversion H3; clear H3.
+    rewrite <- ap_cSS_e.
+    rewrite cSS_subst1.
+    split.
+    apply H.
+    apply H4.
+  }
+
+  (* uniformval *)
+  unfold MeasRel.
   intros.
-  unfold Fun_Thm_Val.
-  intros.
-  simpl.
-  unfold FunRel.
-  exists (ap_Se (shift_subst s) e).
+  unfold uniformop.
+  exists (constexp _ (unit_real u)).
   split.
-  reflexivity.
+  apply evs_unif.
+  unfold EqReal.
+  auto.
 
-  intros.
-
-  assert (Rel_Env (cons_env a1 r) (cons_subst val2 s)).
-  apply extend_env_preserves_Rel.
+  (* bindval *)
+  apply bind_preserves_Rel.
+  specialize (H _ _ H1).
+  specialize (H0 _ _ H1).
+  apply H.
+  specialize (H0 _ _ H1).
   apply H0.
-  apply H1.
-  unfold lift_Rel.
-  destruct (den_exp e (cons_env a1 r)) eqn: eq_e.
-  apply I.
 
-  unfold Fun_Thm_Exp in H.
-  specialize (H _ _ H2).
-  rewrite eq_e in *.
-  simpl in H.
-  inversion H; clear H.
-  exists x.
-  inversion H3; clear H3.
-  rewrite <- ap_cSS_e.
-  rewrite cSS_subst1.
-  split.
+  (* returnval *)
+  apply return_preserves_Rel.
+  specialize (H _ _ H0).
   apply H.
-  apply H4.
-  Qed.
+Qed.
+
 
   
